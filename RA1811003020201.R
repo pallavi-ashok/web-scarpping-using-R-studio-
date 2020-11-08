6519adeca86cfccd8077d899e5db6084ac3892b4
pallavi-ashok <74147318+pallavi-ashok@users.noreply.github.com>
2020-11-08 21:44:47 +0530

    Add files via upload

---
+++ RA1811003020201.R
@@ -0,0 +1,9 @@
+library(robotstxt) 
+path<-paths_allowed("https://www.careerpower.in/cricket-world-cup-winners-list.html")
+library(rvest) 
+url<-"https://www.careerpower.in/cricket-world-cup-winners-list.html" 
+web<-read_html("https://www.careerpower.in/cricket-world-cup-winners-list.html") 
+table<-html_nodes(web,"table")
+result<-html_table(table)[[1]] 
+View(result) 
+write.csv(result,"worldcup_winner.csv")
